537079c14d8e0022cf61e15231a60c9b2d842732
Tim Schwartz <tim@metaverse.systems>
2024-06-13 00:51:33 -0500

    vite updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import ClarionRoutes from "./ClarionRoutes";
+import { ClarionRoutes } from "./ClarionRoutes";
 
 function App() {
 
   return (
     <>
+      <input type="text" id="package" placeholder="Package Name" />
+      <button onClick = {() => {
+        const packageName = (document.getElementById('package')! as HTMLInputElement).value;
+        if (import.meta.hot) import.meta.hot.send('frontend:from-client', { install: packageName });
+        }}>Install</button>
+      <button onClick = {() => {
+        const packageName = (document.getElementById('package')! as HTMLInputElement).value;
+        if (import.meta.hot) import.meta.hot.send('frontend:from-client', { uninstall: packageName });
+        }}>Uninstall</button>
       <ClarionRoutes />
     </>
   )
