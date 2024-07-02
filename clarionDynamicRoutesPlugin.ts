c81ad2d3eb668ae2cd5cbf780b2547791b67bb46
Tim Schwartz <tim@metaverse.systems>
2024-07-02 18:59:04 -0500

    Removed comments

--- clarionDynamicRoutesPlugin.ts
+++ clarionDynamicRoutesPlugin.ts
@@ -4,7 +4,7 @@ import { logger } from './src/logger';
 export const clarionDynamicRoutesPlugin = () => ({
     name: 'clarion-dynamic-routes',
     handleHotUpdate({file, timestamp }) {
-      //split file into path and filename
+      
       const fileParts = file.split('/');
       const filename = fileParts[fileParts.length - 1];
       if(filename !== 'package.json') return;
@@ -13,7 +13,6 @@ export const clarionDynamicRoutesPlugin = () => ({
       logger(`${file} updated`);
     
       const clarionPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-      //store keys from clarionPackage.dependencies in array
       const dependencies = Object.keys(clarionPackage.dependencies);
       const imports = [ 'import { Route, Routes } from "react-router-dom";' ];
       const components = {};
@@ -21,7 +20,6 @@ export const clarionDynamicRoutesPlugin = () => ({
       dependencies.forEach((dependency) => {
         const path = `./node_modules/${dependency}/package.json`;
         const packageJson = JSON.parse(fs.readFileSync(path, 'utf8'));
-        // check customFields for routes key
         if(packageJson.customFields && packageJson.customFields.routes) {
           const routes = packageJson.customFields.routes;
           routes.forEach((route) => {
@@ -46,7 +44,6 @@ export const clarionDynamicRoutesPlugin = () => ({
       output += '    </Routes>\n';
       output += '  );\n';
       output += '};\n';
-      // write imports to src/ClarionRoutes.tsx
       fs.writeFileSync('./src/ClarionRoutes.tsx', output, 'utf8');
     }
   });
