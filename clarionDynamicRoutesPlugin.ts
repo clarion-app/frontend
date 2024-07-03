2632742ef4d78ea73ea3b157b8952f37bec1a373
Tim Schwartz <tim@metaverse.systems>
2024-07-02 21:23:46 -0500

    This commit adds the `clarionDynamicMenu` and `clarionDynamicRoutes` functions to handle dynamic menu and routes generation based on the `package.json` file and its dependencies. The `clarionDynamicMenu` function generates a dynamic menu component, while the `clarionDynamicRoutes` function generates dynamic routes using the `react-router-dom` library. These functions are used in the `clarionDynamicMenuPlugin.ts` and `clarionDynamicRoutesPlugin.ts` files respectively.

--- clarionDynamicRoutesPlugin.ts
+++ clarionDynamicRoutesPlugin.ts
@@ -1,49 +1,8 @@
-import * as fs from 'fs';
-import { logger } from './src/logger';
+import { clarionDynamicRoutes } from './clarionDynamicRoutes';
 
 export const clarionDynamicRoutesPlugin = () => ({
     name: 'clarion-dynamic-routes',
     handleHotUpdate({file, timestamp }) {
-      
-      const fileParts = file.split('/');
-      const filename = fileParts[fileParts.length - 1];
-      if(filename !== 'package.json') return;
-  
-      console.log(`${timestamp}: ${file} updated`);
-      logger(`${file} updated`);
-    
-      const clarionPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-      const dependencies = Object.keys(clarionPackage.dependencies);
-      const imports = [ 'import { Route, Routes } from "react-router-dom";' ];
-      const components = {};
-  
-      dependencies.forEach((dependency) => {
-        const path = `./node_modules/${dependency}/package.json`;
-        const packageJson = JSON.parse(fs.readFileSync(path, 'utf8'));
-        if(packageJson.customFields && packageJson.customFields.routes) {
-          const routes = packageJson.customFields.routes;
-          routes.forEach((route) => {
-            const component = route.element.replace(" ", "").replace("<", "").replace("/>", "");
-            components[component] = route.path;
-          });
-          const importStatement = `import { ${Object.keys(components).join(', ')} } from "${dependency}";`;
-          imports.push(importStatement);
-        }
-      });
-
-      components['div'] = '/';  // temporary fix for root route
-  
-      let output = imports.join('\n');
-      output += '\n\n';
-      output += 'export const ClarionRoutes = () => {\n';
-      output += '  return (\n';
-      output += '    <Routes>\n';
-      Object.keys(components).forEach((component) => {
-        output += `      <Route path="${components[component]}" element={<${component} />} />\n`;
-      });
-      output += '    </Routes>\n';
-      output += '  );\n';
-      output += '};\n';
-      fs.writeFileSync('./src/ClarionRoutes.tsx', output, 'utf8');
+      clarionDynamicRoutes(file, timestamp);
     }
   });
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 import { clarionDynamicRoutesPlugin } from './clarionDynamicRoutesPlugin';
 import { clarionInstallPackagePlugin } from './clarionInstallPackagePlugin';
 import { clarionDynamicMenuPlugin } from './clarionDynamicMenuPlugin';
+import { clarionDynamicRoutes } from './clarionDynamicRoutes';
+import { clarionDynamicMenu } from './clarionDynamicMenu';
 
+export const devSetupPlugin = () => ({
+  name: 'dev-setup-plugin',
+  configureServer() {
+    clarionDynamicRoutes('./package.json', Date.now());
+    clarionDynamicMenu('./package.json', Date.now());
+  }
+});
 
 
 // https://vitejs.dev/config/
@@ -12,6 +21,7 @@ export default defineConfig({
     react(),
     clarionDynamicRoutesPlugin(),
     clarionInstallPackagePlugin(),
-    clarionDynamicMenuPlugin()
+    clarionDynamicMenuPlugin(),
+    devSetupPlugin()
   ]
 })
