8a84be0a24b3509e36f31814e99af076f17829bc
Tim Schwartz <tim@metaverse.systems>
2024-07-02 21:05:51 -0500

    refactor: Refactor package installation and uninstallation logic
    Simplify the code in the `App.tsx` file by extracting the package installation and uninstallation logic into a separate function called `packageAction`. This function takes an action (install or uninstall) and a package name as parameters. It checks if the `import.meta.hot` object exists and sends a message to the client with the action and package name. This improves code readability and maintainability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { ClarionRoutes } from "./ClarionRoutes";
 import { ClarionMenu } from "./ClarionMenu";
 
+const packageAction = (action: string, packageName: string) => {
+  if (!import.meta.hot) return;
+  if(packageName.length === 0) return;
+  import.meta.hot.send('frontend:from-client', { [action]: packageName }); 
+}
+
 function App() {
   return (
     <>
       <ClarionMenu />
       <input type="text" id="package" placeholder="Package Name" />
       <button onClick = {() => {
-        const packageName = (document.getElementById('package')! as HTMLInputElement).value;
-        if (import.meta.hot) import.meta.hot.send('frontend:from-client', { install: packageName });
+        const packageName = (document.getElementById('package') as HTMLInputElement).value;
+        packageAction('install', packageName);
         }}>Install</button>
       <button onClick = {() => {
-        const packageName = (document.getElementById('package')! as HTMLInputElement).value;
-        if (import.meta.hot) import.meta.hot.send('frontend:from-client', { uninstall: packageName });
+        const packageName = (document.getElementById('package') as HTMLInputElement).value;
+        packageAction('uninstall', packageName);
         }}>Uninstall</button>
       <ClarionRoutes />
     </>
