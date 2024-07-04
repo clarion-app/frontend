433d685c4158540ca3a2c114f9a0b6cb1c2386e9
Tim Schwartz <tim@metaverse.systems>
2024-07-04 17:41:00 -0500

    Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { ClarionRoutes } from "./ClarionRoutes";
 import { ClarionMenu } from "./ClarionMenu";
 
-// Tells the dev server to install or uninstall a package.
-const packageAction = (action: string, packageName: string) => {
-  if (!import.meta.hot) return;
-  if(packageName.length === 0) return;
-  import.meta.hot.send('frontend:from-client', { [action]: packageName }); 
-}
-
 function App() {
+  // Tells the dev server to install or uninstall a package.
+  const packageAction = (action: string) => {
+    if (!import.meta.hot) return;
+    const packageName = (document.getElementById('package') as HTMLInputElement).value;
+    if(packageName.length === 0) return;
+    import.meta.hot.send('frontend:from-client', { [action]: packageName }); 
+  };
+  
   return (
     <>
       <ClarionMenu />
       <ClarionRoutes />
       <input type="text" id="package" placeholder="Package Name" />
-      <button onClick = {() => {
-        const packageName = (document.getElementById('package') as HTMLInputElement).value;
-        packageAction('install', packageName);
-        }}>Install</button>
-      <button onClick = {() => {
-        const packageName = (document.getElementById('package') as HTMLInputElement).value;
-        packageAction('uninstall', packageName);
-        }}>Uninstall</button>
+      <button onClick = {() => packageAction('install')}>Install</button>
+      <button onClick = {() => packageAction('uninstall')}>Uninstall</button>
     </>
   )
 }
