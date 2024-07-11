4d38bdb21531b827a04f957acda5d34b6a33b313
Tim Schwartz <tim@metaverse.systems>
2024-07-10 19:08:17 -0500

    refactor: Update Laravel Echo event listeners in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,28 @@ import { WindowWS } from "./types";
 
 function App() {
   useEffect(() => {
-  const win = window as unknown as WindowWS;
+    const win = window as unknown as WindowWS;
     if (win.Echo) {
-      win.Echo.channel('clarion-apps')
-          .listen('.ClarionApp\\Backend\\Events\\InstallNPMPackageEvent', (e: any) => {
-              console.log('Package installed:', e.package);
-          })
-          .listen('.ClarionApp\\Backend\\Events\\UninstallNPMPackageEvent', (e: any) => {
-              console.log('Package uninstalled:', e.package);
-          });
-  }
+        win.Echo.channel('clarion-apps')
+            .listen('.ClarionApp\\Backend\\Events\\InstallNPMPackageEvent', (e: any) => {
+              console.log('Installing:', e.package);
+              if (!import.meta.hot) return;
+              if(!e.package || e.package.length === 0) return;
+              import.meta.hot.send('frontend:from-client', { "install": e.package });
+            })
+            .listen('.ClarionApp\\Backend\\Events\\UninstallNPMPackageEvent', (e: any) => {
+              console.log('Uninstalling:', e.package);
+              if (!import.meta.hot) return;
+              if(!e.package || e.package.length === 0) return;
+              import.meta.hot.send('frontend:from-client', { "uninstall": e.package });
+            });
+    }
 
-  return () => {
-      if (win.Echo) {
-          win.Echo.leaveChannel('clarion-apps');
-      }
-  };
+    return () => {
+        if (win.Echo) {
+            win.Echo.leaveChannel('clarion-apps');
+        }
+    };
   }, []);
   return (
     <>
