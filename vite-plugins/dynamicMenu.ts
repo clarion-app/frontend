b2822ffc0870972b2d439623414b9184ee8be330
Tim Schwartz <tim@metaverse.systems>
2024-07-12 19:04:38 -0500

    Updated comments

--- vite-plugins/dynamicMenu.ts
+++ vite-plugins/dynamicMenu.ts
@@ -3,10 +3,10 @@ import * as fs from 'fs';
 /* 
  * This function is used to dynamically generate a menu for the Clarion app.
  * It reads the package.json file and looks for dependencies that have customFields.routes in their package.json.
- * It then generates a src/build/Menu.tsx file that contains the menu for the Clarion app.
- * The generated file is used by App.tsx
+ * It then generates a src/build/menu.json file that contains the menu entries for the Clarion app.
+ * The generated file is used by src/Menu.tsx.
  * The function is called by the dynamicRebuildPlugin when package.json is updated.
- * The function is also called by the devSetupPlugin to ensure the ClarionMenu.tsx file exists before starting the server.
+ * The function is also called by the devSetupPlugin to ensure the menu.json file exists before starting the server.
  */
 
 export const dynamicMenu = () => {
